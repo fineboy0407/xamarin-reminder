9ebbf0aaaca147734394e152fc2aa933087dbedb
fineboy0407 <levilee.fine@gmail.com>
2018-11-10 23:43:57 +0300

    Web client: Login, Register and Index pages separated.

--- Reminder/Reminder.WebApi/Controllers/HomeController.cs
+++ Reminder/Reminder.WebApi/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Reminder.WebApi.Controllers
 {
@@ -11,7 +7,18 @@ namespace Reminder.WebApi.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
+            return View();
+        }
 
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
             return View();
         }
     }
